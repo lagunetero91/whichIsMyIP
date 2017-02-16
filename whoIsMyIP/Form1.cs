98e54f95df991195b93d5bfc0448a10f917f7dd6
David Leon Ortega <david_lagu@hotmail.com>
2017-02-16 21:08:31 +0000

    Primer commit
    Programa en C# que muestra la ip del host donde se ha ejecutado.

---
+++ whoIsMyIP/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace whoIsMyIP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            getMyIP();
+        }
+
+        private void getMyIP()
+        {
+            IPHostEntry host = Dns.Resolve(Dns.GetHostName());
+            IPAddress ip = host.AddressList[0];
+            MessageBox.Show("My IP is: " + ip);
+        }
+    }
+}
